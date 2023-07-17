40b3b4d1c421a2be8b0dcbc953afd2f532c5f9e7
or-ziv <orzi19970@gmail.com>
2023-07-17 21:19:11 +0300

    changes

--- movies/src/components/TopRatedMovies.jsx
+++ movies/src/components/TopRatedMovies.jsx
@@ -20,12 +20,11 @@ export default function TopRatedMovies() {
                             <img
                                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                                 alt={movie.title} />
-                                
-                            <div className="flex" style={{flexDirection:'row'}}>
-                                <h3>{movie.vote_average}</h3>
-                                <img src={starIcon} alt="starIcon" style={{ width: '20px' , border:'none'}} />
-                            </div>
 
+                            <div className="flex" style={{ flexDirection: 'row' }}>
+                                <h3>{movie.vote_average}</h3>
+                                <img src={starIcon} alt="starIcon" style={{ width: '20px', border: 'none', marginLeft: '10px' }} />
+                            </div>
                             <br />
                         </div>
                     )
